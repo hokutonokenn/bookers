511ca628adbfd16c328025b20d80374ad34225e1
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-08-09 02:00:09 +0000

    task1-4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @books = Book.new
-  end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
 
   def index
     @books = Book.all
+    @book = Book.new(book_params)
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(book.id)
+    else
+       render :new
+    end
   end
 
   def edit
@@ -22,14 +24,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(book.id)
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destoy
+    @book = Book.find(params[:id])
+    @book.destoy
     redirect_to '/books'
   end
 
