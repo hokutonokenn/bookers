4d781a9a7b569e900e26183f6daff91e1aaf6cbd
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-08-09 05:42:45 +0000

    task1-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
   def show
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-       redirect_to book_path(book.id)
+       redirect_to book_path(@book)
     else
        render :new
     end
