a906f47ab15a30addec51237350e917f2d371c95
hokutonokenn <108508259+hokutonokenn@users.noreply.github.com>
2022-08-12 12:31:19 +0000

    [Finish]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully updated."
@@ -25,10 +26,14 @@ class BooksController < ApplicationController
   end
 
   def update
+    @books = Book.all
     @book = Book.find(params[:id])
-    @book.update(book_params)
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
     redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
